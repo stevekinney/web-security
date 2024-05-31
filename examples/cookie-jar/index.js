78c00fb643eb772f4010d5b349f603a3dbeffcbf
Steve Kinney <hello@stevekinney.net>
2024-05-31 11:05:39 -0500

    Follow-along the cookie jar example

--- examples/cookie-jar/index.js
+++ examples/cookie-jar/index.js
@@ -1,9 +1,18 @@
 import { createServer, startServer } from '#shared';
 import { readFile } from 'fs/promises';
+import cookieParser from 'cookie-parser';
+import crypto from 'crypto';
 
 import db from './database.js';
 
-const app = createServer({ cookies: false });
+const cookieSecret = 'super-secret';
+
+const app = createServer({ cookies: false, log: false });
+app.use(cookieParser(cookieSecret));
+
+const generateSessionId = () => {
+  return crypto.randomBytes(16).toString('hex');
+};
 
 app.get('/', (req, res) => {
   if (!req.cookies) res.send('Cookies are disabled.');
@@ -17,7 +26,7 @@ app.get('/', (req, res) => {
 app.get('/login', async (req, res) => {
   const loginPage = await readFile('./pages/login.html', 'utf-8');
 
-  if (req.cookies.username) {
+  if (req.signedCookies.username) {
     res.redirect('/profile');
   }
 
@@ -41,7 +50,18 @@ app.post('/login', async (req, res) => {
   );
 
   if (user) {
-    res.cookie('username', username);
+    const sessionId = generateSessionId();
+    await db.run('INSERT INTO sessions (id, username) VALUES (?, ?)', [
+      sessionId,
+      username,
+    ]);
+
+    res.cookie('sessionId', sessionId, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      signed: true,
+    });
+
     res.redirect('/profile');
   } else {
     res.status(403).redirect('/login?error=Invalid login credentials.');
@@ -57,13 +77,23 @@ app.post('/logout', (_, res) => {
 app.get('/profile', async (req, res) => {
   res.locals.title = 'Profile';
 
-  const username = req.cookies.username;
+  const sessionId = req.signedCookies.sessionId;
 
-  if (!username) {
+  if (!sessionId) {
     return res.redirect('/login?error=Please login to view your profile.');
   }
 
-  const user = await db.get('SELECT * FROM users WHERE username = ?', username);
+  const session = await db.get(
+    'SELECT * FROM sessions WHERE id = ?',
+    sessionId
+  );
+
+  const user = await db.get(
+    'SELECT * FROM users WHERE username = ?',
+    session.username
+  );
+
+  console.log(user, session);
 
   if (user && user.username) {
     res.send(
